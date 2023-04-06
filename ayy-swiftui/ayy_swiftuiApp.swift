06d2e7e2c86845e384f1053b31622013820043e7
zhangqian <zhangqian@jlife.vip>
2023-04-06 10:43:30 +0800

    Initial Commit

---
+++ ayy-swiftui/ayy_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ayy_swiftuiApp.swift
+//  ayy-swiftui
+//
+//  Created by iMac on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct ayy_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
